fef00be461d84dd0d57f34311831998eaa68c567
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-08-22 13:52:21 +0530

    print10usingif2.java

---
+++ print10usingif2.java
@@ -0,0 +1,15 @@
+package NewPack;
+
+public class print10usingif2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=50;
+if(true)
+{
+	System.out.println("this program runs"+num);
+}
+System.out.println("program continues");
+	}
+
+}
