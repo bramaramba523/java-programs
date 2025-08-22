76ce3db3fae875ecc3673e28624f5f16f6eea0d9
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-08-22 13:49:46 +0530

    Add files via upload
    logic

---
+++ print10usingif1.java
@@ -0,0 +1,24 @@
+package NewPack;
+
+public class print10usingif1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b=20,c=20;
+if(a>b && a>c)
+{
+	System.out.println("a is greatest");
+}
+else if(b>a && c>b)
+{
+	System.out.println("b is greatest");
+}
+else if(c>a && c>b)
+{
+	System.out.println("c is greatest");
+	
+}
+System.out.println("program continues");
+	}
+
+}
