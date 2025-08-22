0ab3331d20a0726fb671c6a0a80aa1bba166dff7
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-08-22 14:00:04 +0530

    print10usingif.java

---
+++ print10usingif.java
@@ -0,0 +1,18 @@
+package NewPack;
+
+public class print10usingif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int age=34;
+  if(age>18)//true
+  {
+	  System.out.println("eligible to vote");
+	  }
+  else//false
+  {
+	  System.out.println("not eligible to vote");
+  }
+	}
+
+}
